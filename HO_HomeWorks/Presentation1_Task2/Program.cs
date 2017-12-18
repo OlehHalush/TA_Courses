2bd61d26a4db9cecb36a1e0a747124a078d10840
oleg galush <oleggalushh@gmail.com>
2017-12-18 18:20:48 +0200

    Presentation1_Task2 Done

---
+++ HO_HomeWorks/Presentation1_Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Output question “How are you?“. 
+Define string variable answer.
+Read the value answer and output: “You are (answer)". 
+*/
+
+namespace Presentation1_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How are you?");
+            string answer = Console.ReadLine();
+            Console.WriteLine("You are {0}.", answer);
+            Console.ReadKey();
+        }
+    }
+}
