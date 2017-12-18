7bb65b4f0f95dae54a0e9d5dbd81cbd43cac2a9e
Oleg Galush <oleggalushh@gmail.com>
2017-12-19 01:46:29 +0200

    Presentation3_Task1_1 Done

---
+++ HO_HomeWorks/Presentation3_Task1_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Ввести два цілих числа day та month і перевірити чи можуть вони представляти день та місяць.
+Вивести true чи false
+*/
+
+namespace Presentation3_Task1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter first number:");
+            int day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter socond number:");
+            int month = Convert.ToInt32(Console.ReadLine());
+            bool canBeADay = false;
+            bool canBeAMonth = false;
+            if (day > 0 && day < 32)
+                canBeADay = true;
+            if (month > 0 && month < 13)
+                canBeAMonth = true;
+            Console.WriteLine("Can your first number be a day? - {0}", canBeADay);
+            Console.WriteLine("Can your second number be a month? - {0}", canBeAMonth);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HO_HomeWorks/Presentation3_Task1_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Ввести дійсне число  number і отримати 2 перші цифри після коми цього числа.
+Вивести суму цих цифр. Напр.: 3.456->4+5=9
+*/
+
+namespace Presentation3_Task1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter double value:");
+            double userInput = Convert.ToDouble(Console.ReadLine());
+            userInput /= 1;
+            string userNumber = Convert.ToString(userInput);
+            char[] charArray = new char[2];
+            char first = userNumber[2];
+            char second = userNumber[3];
+            int firstNumber = Convert.ToInt32(first);
+            int secondNumber = Convert.ToInt32(second);
+            int result = first + second;
+            Console.WriteLine(first);
+            Console.ReadKey();
+        }
+    }
+}
