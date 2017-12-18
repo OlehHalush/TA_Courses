eabd748e8696c7fd2e0445f7f5591a26963310e0
oleg galush <oleggalushh@gmail.com>
2017-12-18 18:31:41 +0200

    Presentation1_Task3 Done

---
+++ HO_HomeWorks/Presentation1_Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Read 3 variables of char type. 
+Write message: “You enter (first char), (second char), (3 char)”
+*/
+namespace Presentation1_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first char:");
+            char firstChar = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter second char:");
+            char secondChar = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter third char:");
+            char thirdChar = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Your first char is: {0}", firstChar);
+            Console.WriteLine("Your second char is: {0}", secondChar);
+            Console.WriteLine("Your third char is: {0}", thirdChar);
+            Console.ReadKey();
+        }
+    }
+}
