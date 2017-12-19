73fa02af303d7902e923b74153877dcefd7192b2
Oleg Galush <oleggalushh@gmail.com>
2017-12-19 23:05:05 +0200

    Presentation3_Task2_1

---
+++ HO_HomeWorks/Presentation3_Task2_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Визначити перелічення TestCaseStatus (Pass, Fail, Blocked, WP, Unexecuted). 
+Змінній test1Status присвоїти значення Pass і вивести значення змінної на консоль.
+*/
+
+namespace Presentation3_Task2_1
+{
+    enum TestCaseStatus : byte
+    {
+        Pass,
+        Fail,
+        Blocked,
+        WP,
+        Unexecuted
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestCaseStatus test1Status = TestCaseStatus.Pass;
+            Console.WriteLine(test1Status);
+            Console.ReadLine();
+        }
+    }
+}
