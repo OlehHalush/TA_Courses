20028cd595397d02c75426fea16c577084296c32
Oleg Galush <oleggalushh@gmail.com>
2017-12-28 00:06:15 +0200

    Task6_1_6_2

---
+++ HO_HomeWorks/Presentation7_Task6_1+6_2/MyClass.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Presentation7_Task6_1_6_2
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+
+        public MyClass(string pathToRead, string pathToWrite)
+        {
+            this.pathToRead = @pathToRead;
+            this.pathToWrite = @pathToWrite;
+        }
+
+        private string pathToRead = @"C:\data.txt";
+        private string pathToWrite = @"C:\rez.txt";
+        private double number1;
+        private double number2;
+        private double result;
+
+        public double Result
+        {
+            get
+            {
+                return this.result;
+            }
+        }
+
+        public void ReadFromFile()
+        {
+            string userInput1;
+            string userInput2;
+            using (StreamReader reader = new StreamReader(@pathToRead))
+            {
+                userInput1 = reader.ReadLine();
+                userInput2 = reader.ReadLine();
+            }
+
+            this.number1 = double.Parse(userInput1);
+            this.number2 = double.Parse(userInput2);
+        }
+
+        public void Divide()
+        {
+            if (this.number2 == 0)
+            {
+                throw new ArgumentNullException("Division by 0 is not allowed.");
+            }
+
+            this.result = this.number1 / this.number2;
+            Console.WriteLine("The result of division {0} by {1} is {2}", this.number1, this.number2, this.Result);
+        }
+
+        public void OutputIntoFile()
+        {
+            using (StreamWriter writer = new StreamWriter(@pathToWrite))
+            {
+                writer.WriteLine("Number one is: {0}.", this.number1);
+                writer.WriteLine("Number two is: {0}", this.number2);
+                writer.WriteLine("The result of division is: {0}.", this.Result);
+            }
+        }
+    }
+}
---
+++ HO_HomeWorks/Presentation7_Task6_1+6_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+/*
+1) Create method Div(), which calculates the dividing of two  double numbers. 
+In Main input  2 double numbers and call this method. Catch appropriative exceptions. 
+2) Read and write all data in task1 from data.txt into rez.txt files, by using (Catch appropriative exceptions):
+StreamReader StreamWriter
+File.WriteAllText
+*/
+
+namespace Presentation7_Task6_1_6_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyClass myProgram = new MyClass();
+            try
+            {
+                myProgram.ReadFromFile();
+                myProgram.Divide();
+                myProgram.OutputIntoFile();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
