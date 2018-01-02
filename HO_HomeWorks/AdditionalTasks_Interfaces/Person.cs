2054aeb01da903f610e3659a23324ec86e2c01fd
Oleg Galush <oleggalushh@gmail.com>
2018-01-02 23:22:19 +0200

    AdditionalTasks_CollectionsAndInterfaces

---
+++ HO_HomeWorks/AdditionalTasks_Interfaces/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalTasks_Interfaces
+{
+    public class Person : ICloneable
+    {
+        public Person()
+        {
+            this.name = "Not Defined";
+            this.age = 0;
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                this.age = value;
+            }
+        }
+
+        public object Clone()
+        {
+            Person newPerson = this;
+            return newPerson;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine("Name is {0}, age is {1}.", this.name, this.age);
+        }
+    }
+}
---
+++ HO_HomeWorks/AdditionalTasks_Interfaces/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Для розробленого раніше класу (напр. Doctor, Car, Person) реалізувати інтерфейс IClonable для клонування об’єктів.
+Перевірити роботу методу Clone(), створюючи і клонуючи 10 об’єктів класу (використати конструктор для створення об’єкта лише 1 раз, решту об’єктів створити клонуванням)
+*/
+
+namespace AdditionalTasks_Interfaces
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> myListOfPersons = new List<Person>();
+            Person myPerson = new Person("Oleg", 28);
+            for (int i = 0; i < 9; i++)
+            {
+                myListOfPersons.Add((Person)myPerson.Clone());
+            }
+
+            foreach (Person somePerson in myListOfPersons)
+            {
+                somePerson.Output();
+            }
+        }
+    }
+}
