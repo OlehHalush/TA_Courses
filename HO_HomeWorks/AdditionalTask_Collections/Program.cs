2f070740c72fadb00c514f5f91fbb3a9ddad8d54
Oleg Galush <oleggalushh@gmail.com>
2018-01-02 22:47:18 +0200

    AdditionalTasks_Collections

---
+++ HO_HomeWorks/AdditionalTask_Collections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+	Задані натуральне число п і символи s_1,…,s_n, серед яких є двокрапка.
+    Переписати в інший список всі символи, розміщені між першою і другою двокрапкою.
+    Якщо другої двокрапки немає,то переписати всі символи, розміщені після єдиної двокрапки. 
+    Повторити з наступними двокрапками в тексті
+*/
+
+namespace AdditionalTask_Collections
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TestClass someObject = new TestClass("Oleg:You:Are:My:Friend", ':');
+            someObject.DevideIntoStringByChar();
+            someObject.Output();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HO_HomeWorks/AdditionalTask_Collections/TestClass.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalTask_Collections
+{
+    public class TestClass
+    {
+        public TestClass()
+        {
+        }
+
+        public TestClass(string myString, char devisionChar)
+        {
+            this.myString = myString;
+            this.devisionChar = devisionChar;
+        }
+
+        private string myString;
+        private char devisionChar;
+        private List<string> myListOfStrings = new List<string>();
+
+        public string MyString
+        {
+            get
+            {
+                return this.MyString;
+            }
+        }
+
+        public char DevisionChar
+        {
+            get
+            {
+                return this.devisionChar;
+            }
+        }
+
+        public List<string> MyListOfStrings
+        {
+            get
+            {
+                return this.myListOfStrings;
+            }
+        }
+
+        public List<string> DevideIntoStringByChar()
+        {
+            string[] stringArray = this.myString.Split(this.devisionChar);
+            foreach (string someString in stringArray)
+            {
+                this.myListOfStrings.Add(someString);
+            }
+
+            return this.myListOfStrings;
+        }
+
+        public List<string> DevideIntoStringByChar(string myString, char myChar)
+        {
+            Console.WriteLine("Here is a list of strings devided by '{0}'.", myChar);
+            string[] stringArray = myString.Split(myChar);
+            foreach (string someString in stringArray)
+            {
+                this.myListOfStrings.Add(someString);
+            }
+
+            return this.myListOfStrings;
+        }
+
+        public void Output()
+        {
+            foreach (string someString in this.myListOfStrings)
+            {
+                Console.WriteLine(someString);
+            }
+        }
+    }
+}
