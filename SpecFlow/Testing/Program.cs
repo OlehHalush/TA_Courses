ee701e1b3b3fdc79d6edade06d95e7e2221f534e
SOFTSERVE\ohalu <oleggalushh@gmail.com>
2018-01-04 15:44:47 +0200

    SpecFlowStart

---
+++ SpecFlow/Testing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SpecFlow/Testing/Steps/Steps.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace Testing.Steps
+{
+    [Binding]
+    public class Steps
+    {
+        string name;
+        [When(@"I write ""(.*)""")]
+        public void WhenIWrite(string name)
+        {
+            this.name = name;
+        }
+
+        [Then(@"it is equal to ""(.*)""")]
+        public void CheckName(string name)
+        {
+            Assert.That(this.name,Is.EqualTo(name));
+        }
+    }
+}
