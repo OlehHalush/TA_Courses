68bb55a5b301c152cbd5d8d815bde0fbb4e542ba
Oleg Galush <oleggalushh@gmail.com>
2018-01-10 00:49:17 +0200

    asd

---
+++ HO_HomeWorks/Task6_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+/*
+Write into file ‘DirectoryC.txt’ information(name, type, size) about all directories and files from disc C on your computer.Catch appropriative exceptions.
+*/
+
+namespace Task6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dirs = Directory.GetDirectories(@"C:", "*", SearchOption.AllDirectories);
+            foreach (string item in dirs)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
