fcab1804cc8ced42f7ab32d1912bcdc30b8ac849
Oleg Galush <ohalu@softserveinc.com>
2018-02-06 22:59:51 +0200

    Appium

---
+++ HO_HomeWorks/AppiumTest/AppiumTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Remote;
+
+namespace AppiumTest
+{
+    [TestFixture]
+    class AppiumTest
+    {
+        [Test]
+        public static void CheckAppiumAndroid()
+        {
+            AppiumDriver<AndroidElement> driver;
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.SetCapability(MobileCapabilityType.App, @"D:\ApiDemos-debug.apk");
+            capabilities.SetCapability(MobileCapabilityType.DeviceName, "emulator -5554");
+            capabilities.SetCapability(MobileCapabilityType.Udid, "emulator-5554");
+            capabilities.SetCapability(MobileCapabilityType.PlatformVersion, "6.0.0");
+            capabilities.SetCapability(MobileCapabilityType.PlatformName, "Android");
+            capabilities.SetCapability(MobileCapabilityType.FullReset, "false");
+            driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), capabilities);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        }
+    }
+}
